c8dbe2373ff862e26ee51eb2e124941ad8bcee17
Petros Douvantzis <petrakeas@gmail.com>
2021-04-02 16:14:26 +0300

    Initial commit

---
+++ mylibrary/src/main/java/com/petrakeas/mylibrary/AndroidLib.java
@@ -0,0 +1,20 @@
+package com.petrakeas.mylibrary;
+
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+/**
+ * Documentation
+ */
+public class AndroidLib {
+
+    private Gson gson;
+
+    /**
+     * Documentation
+     */
+    public void test() {
+        Log.d("TEST", "this is a test");
+    }
+}
---
+++ testlib/src/main/java/com/petrakeas/testlib/JavaLib.java
@@ -0,0 +1,17 @@
+package com.petrakeas.testlib;
+
+import com.google.gson.Gson;
+
+/**
+ * This is a class!
+ */
+public class JavaLib {
+
+    private Gson gson;
+
+
+    public JavaLib() {
+
+    }
+
+}
